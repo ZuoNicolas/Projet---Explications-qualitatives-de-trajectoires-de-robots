ea03874bd3e689faa63e5d7fe756a115d5df7e8a
zitongSophie <62668648+zitongSophie@users.noreply.github.com>
2021-02-13 17:06:43 +0100

    Add files via upload

---
+++ ressource/exemple.tsx
@@ -0,0 +1,30 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="exemple" tilewidth="16" tileheight="16" spacing="1" margin="1" tilecount="1320" columns="60">
+ <image source="Downloads/tutoville2_modif.png" width="1021" height="375"/>
+ <terraintypes>
+  <terrain name="terrain" tile="1032">
+   <properties>
+    <property name="canPass" type="bool" value="true"/>
+   </properties>
+  </terrain>
+  <terrain name="point depart" tile="60"/>
+  <terrain name="point arriver" tile="120"/>
+  <terrain name="arbre" tile="1154">
+   <properties>
+    <property name="canPass" type="bool" value="false"/>
+   </properties>
+  </terrain>
+ </terraintypes>
+ <tile id="1032" terrain="0,0,0,0"/>
+ <tile id="1033" terrain="0,0,0,0"/>
+ <tile id="1034" terrain="0,0,0,0"/>
+ <tile id="1092" terrain="3,3,3,3"/>
+ <tile id="1093" terrain="3,3,3,3"/>
+ <tile id="1094" terrain="3,3,3,3"/>
+ <tile id="1152" terrain="3,3,3,3"/>
+ <tile id="1153" terrain="3,3,3,3"/>
+ <tile id="1154" terrain="3,3,3,3"/>
+ <tile id="1212" terrain="0,0,0,0"/>
+ <tile id="1213" terrain="0,0,0,0"/>
+ <tile id="1214" terrain="0,0,0,0"/>
+</tileset>
