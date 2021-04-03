ae623feec2ddf2aac7bb6f35bd04de18919ab632
ZuoNicolas <nicolaszuo.contact@gmail.com>
2021-04-03 11:08:19 +0200

    'a'

---
+++ src/myEnum.py
@@ -0,0 +1,40 @@
+from enum import Enum
+
+class Description(Enum):
+    
+    #Orientation
+    NORD = -1
+    EST = -2
+    SUD = -3
+    OUEST = -4
+    
+    #Action
+    TOURNE = -5
+    PASSE = -6
+    AVANCE = -7
+    
+    #Precision Avancer
+    JUSQU_A = -9
+    
+    #Description Avancer
+    INTERSECTION = -10
+    ARRIVER = -11
+    
+    #Precision Distance
+    PEU = -12
+    TRES = -13
+    
+    #Distance
+    PROCHE = -15
+    LOIN = -16
+    
+    #Direction
+    GAUCHE = -22
+    DROITE = -23
+    DEVANT = -24
+    DERRIERE = -25
+    ENTRE = -26
+    
+    #Objet 
+    OBJECT = -60
+
