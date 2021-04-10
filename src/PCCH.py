9c9447a26192587f6b5b42c4f290f5b570103aee
axelfoltyn <axel.foltyn@gmail.com>
2021-04-10 11:38:58 +0200

    modification du a* pour qu'il renvoit un score infini si pas de chemin

--- src/PCCH.py
+++ src/PCCH.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 
 class Etat:
         visite = dict()
@@ -77,11 +78,11 @@ class Etat:
                         self.ajout(Etat((a,b),self.g+Etat.weight[(a,b)],self, self.goal, self.pos_depart))
             while "Etat.frontiere[0] n'est pas dans visite" :
                 if len(Etat.frontiere)==0:
-                    return []
+                    return [], math.inf
                 e = Etat.frontiere.pop(0)
                 if e.pos not in Etat.visite :
                     return e.evaluer()
-            return []
+            return [], math.inf
 
 
 def a_start(debut, goal, x_max, y_max, wall, weight=dict()):
